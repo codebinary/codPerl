5876943d17247bf109565c4f49b98792d04e0325
James Ontiveros <james.ontiveros86@gmail.com>
2015-12-03 00:41:16 -0500

    Subiendo archivos Perl

--- funciones2.pl
+++ funciones2.pl
@@ -11,3 +11,23 @@ sub multiplicar{
 }
 
 print multiplicar(5) ."\n";
+print multiplicar(4,4) . "\n";
+
+sub imagen(){
+	my(%opciones) = @_;
+	my %default = ('anchura' => 250, 'altura' => 250, 'color_fondo' => 'negro');
+	
+	foreach(keys %default){
+		if($opciones {$_} eq undef){
+			$opciones{$_} = $default{$_};
+		}	
+	}
+	
+	my $resultado = "La anchura de la imagen es: " . $opciones{'anchura'} . "\n";
+	$resultado .= "La altura de la imagen es: " . $opciones{'altura'} . "\n";
+	$resultado .= "La color de fondo de la imagen es: " . $opciones{'color_fondo'} . "\n";
+	
+	return $resultado;
+}
+
+print imagen();
